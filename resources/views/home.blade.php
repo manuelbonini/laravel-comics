d5bb468814e7a5f035eb1ef7f34f23a761d1c238
Manuel Bonini <manuelbonini17@gmail.com>
2021-06-08 22:09:34 +0200

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('title')
+    Homepage
+@endsection
+
+@section('main_section')
+    <h1>SONO LA HOMEpage</h1>
+@endsection
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,7 @@
+<header>
+
+    <div>
+        ciao sono l headefr
+    </div>
+
+</header>
